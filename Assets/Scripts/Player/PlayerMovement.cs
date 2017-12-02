37359178729a014aface6f162d5df2ab542f027b
Joshua Khoo <joshua.khoo@colorado.edu>
2017-12-01 19:04:46 -0700

    changes to player scripts, stopped endless player spawning

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -18,6 +18,17 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
     void Movement(float horizontal, float vertical) {
-        transform.position = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        }
     }
 }
